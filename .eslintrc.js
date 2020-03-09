19e384b962b2e7d10dd684e08e421c8c9c411d8c
Lucas Rosa <moeda.60@gmail.com>
2020-03-09 18:01:38 -0300

    duplicated product

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
         ],
         'import/prefer-default-export': 'off',
         'no-console': ['error', { allow: ['tron'] }],
+        'no-param-reassign': 'off'
     },
 };
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 /**
  * Called when any dispatch from react-redux is fired.
  *
@@ -7,7 +9,20 @@
 export default function cart(state = [], action) {
     switch (action.type) {
         case 'ADD_TO_CART':
-            return [...state, { ...action.product, amount: 1 }];
+            return produce(state, draft => {
+                const productIndex = draft.findIndex(
+                    p => p.id === action.product.id
+                );
+
+                if (productIndex >= 0) {
+                    draft[productIndex].amount += 1;
+                } else {
+                    draft.push({
+                        ...action.product,
+                        amount: 1,
+                    });
+                }
+            });
         default:
             return state;
     }
