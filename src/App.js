333ef66fdafc43beb55cdc75c7c1e78f21226249
Lucas Rosa <moeda.60@gmail.com>
2020-03-09 16:22:15 -0300

    configure redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 import Header from './components/Header';
 import GlobalStyles from './styles/global';
 
+import store from './store';
+
 function App() {
     return (
-        <>
+        <Provider store={store}>
             <BrowserRouter>
                 <Header />
                 <Routes />
             </BrowserRouter>
             <GlobalStyles />
-        </>
+        </Provider>
     );
 }
 
