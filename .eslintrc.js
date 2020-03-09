f52c3e50cfc0f82e2bd49f118fcf145037580692
Lucas Rosa <moeda.60@gmail.com>
2020-03-09 17:40:05 -0300

    add reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
     env: {
         browser: true,
-        es6: true
+        es6: true,
     },
-    extends: ["airbnb", "prettier", "prettier/react"],
+    extends: ['airbnb', 'prettier', 'prettier/react'],
     globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
     parserOptions: {
         ecmaFeatures: {
-            jsx: true
+            jsx: true,
         },
         ecmaVersion: 2018,
-        sourceType: "module"
+        sourceType: 'module',
     },
-    plugins: ["react", "prettier"],
+    plugins: ['react', 'prettier'],
     rules: {
-        "prettier/prettier": "error",
-        "react/jsx-filename-extension": [
-            "warn",
-            { extensions: [".jsx", ".js"]}
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.jsx', '.js'] },
         ],
-        "import/prefer-default-export": "off"
-    }
+        'import/prefer-default-export': 'off',
+        'no-console': ['error', { allow: ['tron'] }],
+    },
 };
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/ReactotronConfig';
 import Routes from './routes';
 import Header from './components/Header';
 import GlobalStyles from './styles/global';
